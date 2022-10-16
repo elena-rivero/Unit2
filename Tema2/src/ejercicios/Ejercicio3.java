e4804c0c2e9a8106ddd6220285c0108704f1a944
Usuario <Usuario@192.168.1.137>
2022-10-16 09:39:25 +0200

    Ejercicio 3 completo

--- Tema2/src/ejercicios/Ejercicio3.java
+++ Tema2/src/ejercicios/Ejercicio3.java
@@ -1,12 +1,21 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// En la variable numero guardaremos el número introducido por el usuario
+		double numero;
 		
-		double numero = 0.25;
+		//Creamos el Scanner
+		Scanner sc = new Scanner(System.in);
 		
+		// Le pedimos al usuario que introduzca un número real por teclado
+		System.out.println("Introduzca un número real:");
+		numero = sc.nextDouble();
+		
+		// Un número casi-cero es aquel que está en el rango (-1, 1) y es distinto de 0
 		if(numero > -1 && numero < 1 && numero!=0) {
 			System.out.println("Es casi-cero");
 		} else
