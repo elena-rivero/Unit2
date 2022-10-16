9ed224af326b8f4b0b9e602da7b3b179d963c6b4
Usuario <Usuario@192.168.1.137>
2022-10-16 09:47:02 +0200

    Ejercicio 5 completo

--- Tema2/src/ejercicios/Ejercicio5.java
+++ Tema2/src/ejercicios/Ejercicio5.java
@@ -1,21 +1,41 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		int num1 = 1, num2 =3, num3 =5;
-		
-		
-		if(num1 > num2 && num2 > num3) {
-			
-		}else if(num1 > num3 && num3 > num2) {
-			
-		} else if(num2 > num1 && num1 > num3) {
-			
+		// num1, num2, num3 son las variables donde guardaremos los valores introducidos
+		// por el usuario
+		int num1, num2, num3;
+
+		// Creamos el Scanner
+		Scanner sc = new Scanner(System.in);
+
+		// Le pedimos al usuario cada número
+		System.out.println("Introduzca el primer número:");
+		num1 = sc.nextInt();
+
+		System.out.println("Introduzca el segundo número:");
+		num2 = sc.nextInt();
+
+		System.out.println("Introduzca el tercer número:");
+		num3 = sc.nextInt();
+
+		if (num1 > num2 && num2 > num3) { // num1 > num2 > num3
+			System.out.println(num1 + ", " + num2 + ", " + num3);
+		} else if (num1 > num3 && num3 > num2) { // num1 > num3 > num2
+			System.out.println(num1 + ", " + num3 + ", " + num2);
+		} else if (num2 > num1 && num1 > num3) { // num2 > num1 > num3
+			System.out.println(num2 + ", " + num1 + ", " + num3);
+		} else if (num2 > num3 && num3 > num1) { // num2 > num3 > num1
+			System.out.println(num2 + ", " + num3 + ", " + num1);
+		} else if (num3 > num1 && num1 > num2) { // num3 > num1 > num2
+			System.out.println(num3 + ", " + num1 + ", " + num2);
+		} else { // num3 > num2 > num1
+			System.out.println(num3 + ", " + num2 + ", " + num1);
 		}
 	}
-	
 
 }
